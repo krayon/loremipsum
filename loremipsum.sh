4bc8307692b875829b3ec840ae161b1f1136e018
Krayon <krayon.git@qdnx.org>
2014-09-16 05:25:58 +0000

    1. Up'd copyright year.

--- loremipsum.sh
+++ loremipsum.sh
@@ -4,7 +4,7 @@
 #
 #/**********************************************************************
 #    Lorum Ipsum
-#    Copyright (C) 2012-2013 DaTaPaX (Todd Harbour t/a)
+#    Copyright (C) 2012-2014 DaTaPaX (Todd Harbour t/a)
 #
 #    This program is free software; you can redistribute it and/or
 #    modify it under the terms of the GNU General Public License
